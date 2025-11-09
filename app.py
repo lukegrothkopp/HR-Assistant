5df4d63722652943c3365ce200911bad2aafff70
lukegrothkopp <lukegrothkopp@gmail.com>
2025-11-08 16:15:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 # LangChain / OpenAI wrappers
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import ChatPromptTemplate
